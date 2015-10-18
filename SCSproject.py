fa3a5482c849a9ae4b62fb3fd441b46d8b0b49d9
ppopops <ppopops@gmail.com>
2015-10-17 22:16:17 -0400

    Classe salle - CRUD

--- SCSproject.py
+++ SCSproject.py
@@ -1,11 +1,78 @@
 from flask import Flask
-
+from MongoDB import Connect
+from Salle import *
 app = Flask(__name__)
 
+db = Connect()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/SCSproject/api/getsalle', methods=['GET'])
+def get_salle():
+    salle = Salle(db)
+    return jsonify({'salle': salle.ReturnSalle()})
+
+@app.route('/SCSproject/api/setsalle', methods=['POST'])
+def create_salle():
+    content = request.get_json(force = True)
+    print content
+
+    sallei = content.get("salle")
+    salle= sallei[0]
+    numeroSalle =  salle.get ("numeroSalle","")
+    etageSalle =  salle.get ("etageSalle","")
+    capaciteSalle =  salle.get ("capaciteSalle","")
+    equipementSalle =  salle.get ("equipementSalle","")
+    idBatiment =  salle.get ("idBatiment","")
+    id = "0"   #contructor will set id
+    salle = Salle( db, numeroSalle ,etageSalle, capaciteSalle, equipementSalle ,idBatiment, id )
+    salle.InsertSalle()
+
+    return jsonify(result={"status": 200})
+
+
+@app.route('/SCSproject/api/getsalleid/<string:salle_id>', methods=['GET'])
+def get_salle_id(salle_id):
+    salle = Salle(db)
+    result = salle.ReturnSalleid(salle_id)
+    print result
+#    result = [{u'idBatiment': 2, u'etageSalle': u'6', u'numeroSalle': u'214', u'capaciteSalle': u'12'}]
+    return jsonify({'salle': result})
+
+@app.route('/SCSproject/api/updatesalle', methods=['POST'])
+def update_salle():
+    content = request.get_json(force = True)
+    print content
+
+    sallei = content.get("salle")
+    salle= sallei[0]
+    numeroSalle =  salle.get ("numeroSalle","")
+    etageSalle =  salle.get ("etageSalle","")
+    capaciteSalle =  salle.get ("capaciteSalle","")
+    equipementSalle =  salle.get ("equipementSalle","")
+    idBatiment =  salle.get ("idBatiment","")
+    id = salle.get ("_id")
+    salle = Salle( db, numeroSalle ,etageSalle, capaciteSalle, equipementSalle ,idBatiment, id )
+    salle.UpdateSalle()
+
+    return jsonify(result={"status": 200})
+
+@app.route('/SCSproject/api/deletesalle', methods=['POST'])
+def delete_salle():
+    content = request.get_json(force = True)
+    print content
+
+    sallei = content.get("salle")
+    salle= sallei[0]
+    numeroSalle =  salle.get ("numeroSalle","")
+    etageSalle =  salle.get ("etageSalle","")
+    capaciteSalle =  salle.get ("capaciteSalle","")
+    equipementSalle =  salle.get ("equipementSalle","")
+    idBatiment =  salle.get ("idBatiment","")
+    id = salle.get("_id")   #contructor will set id
+    salle = Salle( db, numeroSalle ,etageSalle, capaciteSalle, equipementSalle ,idBatiment, id )
+    salle.DeleteSalle()
+
+    return jsonify(result={"status": 200})
 
 
 if __name__ == '__main__':
--- Salle.py
+++ Salle.py
@@ -1,135 +1,117 @@
+import uuid
 from flask import Flask, jsonify, render_template, request, redirect
 import os
 from pymongo import MongoClient
+from MongoDB import Connect
 
-app = Flask(__name__)
+class Salle:
 
-# compteur pour id salle
+    def __init__(self, db, numeroSalle = "0", etageSalle = "0", capaciteSalle="0", equipmentSalle = "0", idBatiment = "0",id = "0"):
 
-#extraction de toutes les salles
-result = []
+        self.collection = db.Salle    # collection Salle dans MongoDB
+        if  id == "0":
+            uniqueid= uuid.uuid4()
+            self.id = str(uniqueid)
+        else :
+            self.id = id
+        self.capaciteSalle = capaciteSalle
+        self.equipementSalle = equipmentSalle
+        self.etageSalle = etageSalle
+        self.numeroSalle = numeroSalle
+        self.idBatiment = idBatiment
 
-#connection a la BD
-def Connect():
-    connection = MongoClient("ds057862.mongolab.com",57862)
-    handle = connection["mongodbhong"]
-    handle.authenticate("admin","root")
-    return handle
+    def InsertSalle(self):
+        item = {"_id" : self.id, "numeroSalle" : self.numeroSalle ,"etageSalle" : self.etageSalle, "capaciteSalle" : self.capaciteSalle, "equipementSalle" : self.equipementSalle, "idBatiment" : self.idBatiment }
+        print item
+        item_id = self.collection.insert_one(item).inserted_id
+        return item_id
+
+    #retourber toutes les salles
+    def ReturnSalle(self):
+        result = []
+        for data in self.collection.find():
+            result.append(data)
+        return result
+
+    def ReturnSalleid(self, id):
+        result = []
+        for data in self.collection.find({"_id": id }):
+            result.append(data)
+        return result
+
+    def UpdateSalle(self):
+        #print {"_id" : self.id, "numeroSalle" : self.numeroSalle ,"etageSalle" : self.etageSalle, "capaciteSalle" : self.capaciteSalle, "equipementSalle" : self.equipementSalle, "idBatiment" : self.idBatiment }
+        self.collection.update({"_id" : self.id}, {"numeroSalle" : self.numeroSalle ,"etageSalle" : self.etageSalle, "capaciteSalle" : self.capaciteSalle, "equipementSalle" : self.equipementSalle, "idBatiment" : self.idBatiment })
+        return
+
+    def DeleteSalle(self):
+        #print {"_id" : self.id, "numeroSalle" : self.numeroSalle ,"etageSalle" : self.etageSalle, "capaciteSalle" : self.capaciteSalle, "equipementSalle" : self.equipementSalle, "idBatiment" : self.idBatiment }
+        self.collection.remove({"_id" : self.id})
+        return
+
+
+    '''
 
 #static variable for idSalle
-def static_vars(**kwargs):
-    def decorate(func):
-        for k in kwargs:
-            setattr(func, k, kwargs[k])
-        return func
-    return decorate
+    def static_vars(**kwargs):
+        def decorate(func):
+            for k in kwargs:
+                setattr(func, k, kwargs[k])
+            return func
+        return decorate
 
-@static_vars (idSalle= 0)
-def InsertSalle(numeroSalle, etageSalle, capaciteSalle, equipementSalle, idBatiment):
-    InsertSalle.idSalle += 1
-    item = {"_id" : InsertSalle.idSalle , "numeroSalle" : numeroSalle,"etageSalle" : etageSalle, "capaciteSalle" : capaciteSalle, "equipementSalle" : equipementSalle, "idBatiment" : idBatiment }
-    item_id = collection.insert_one(item).inserted_id
-    return item_id
-
-
-def ReturnSalle():
-    with app.app_context():
-        result = []
-        for data in collection.find():
-	        result.append(data)
-        return result
-
-def ReturnSalleid(id):
-    with app.app_context():
-        result = []
-        for data in collection.find({"_id": id }):
-	        result.append(data)
-        return result
-#        result =  collection.find({"_id": id })
-#        print result
-#        print result.next().numeroSalle
-#        return result
+    @static_vars (idSalle= 0)
+    def InsertSalle(numeroSalle, etageSalle, capaciteSalle, equipementSalle, idBatiment):
+        InsertSalle.idSalle += 1
+        item = {"_id" : InsertSalle.idSalle , "numeroSalle" : numeroSalle,"etageSalle" : etageSalle, "capaciteSalle" : capaciteSalle, "equipementSalle" : equipementSalle, "idBatiment" : idBatiment }
+        item_id = collection.insert_one(item).inserted_id
+        return item_id
 
 
-def InsertNomFaculte2 (nomUniversite , nomFaculte):
-    collection.update({"nomUniversite" : nomUniversite}, {"$addToSet":{"Faculte" : {"nomFaculte" : nomFaculte} } })
-
-def InsertNomFaculte (nomUniversite , nomFaculte):
-#    collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte.nomFaculte" : nomFaculte } })
-    collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte." + nomFaculte : nomFaculte } })
-
-def InsertBatiment2 (nomUniversite , nomFaculte, nomBatiment, adresseBatiment):
-# add check nom existe pas deja
-#    collection.update({"nomUniversite" : nomUniversite, "Faculte.nomFaculte": nomFaculte }, {"$push":{"(Faculte.1.nomFaculte)" : {"nomBatiment" : nomBatiment} } })
-#    collection.update({"Faculte.nomFaculte": nomFaculte }, {"$push":{"(Faculte.1.nomFaculte)" : {"nomBatiment" : nomBatiment} } })
-#    collection.update({"Faculte.nomFaculte": nomFaculte }, {"$push":{"Faculte" : { "Sciences" : { "nomBatiment" : nomBatiment} } } })
-    collection.update({"Faculte.nomFaculte": nomFaculte }, {"$push":{"Faculte" : { "elemMatch" : { "$in":  [1] } }} })
-
-def InsertBatiment (nomUniversite , nomFaculte, nomBatiment, adresseBatiment):
-#    collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte.nomFaculte" : nomFaculte } })
-    collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte." + nomFaculte + "." + nomBatiment : nomBatiment } })
-
-####################DRIVER###############################################
-db = Connect()
-collection  = db.Salle
-
-
-#Insert Salle
-id = InsertSalle("214", "6", "12", "projecteur", 2)
-id = InsertSalle("231", "12", "5", "", 2)
-#id = InsertSalle("2432214", "5", "78", "", 2)
-#id = InsertSalle("433", "6", "44", "projecteur", 8)
-#id = InsertSalle("242214", "4", "25", "ordinateur", 10)
-
-
-#ReturnSalle()
-
-@app.route('/SCSproject/api/getsalle', methods=['GET'])
-def get_salle():
-    result = ReturnSalle()
-    print result
-#    result = [{u'idBatiment': 2, u'etageSalle': u'6', u'numeroSalle': u'214', u'capaciteSalle': u'12'}]
-    return jsonify({'salle': result})
-
-@app.route('/SCSproject/api/getsalleid/<int:salle_id>', methods=['GET'])
-def get_salle_id(salle_id):
-    result = ReturnSalleid(salle_id)
-    print result
-#    result = [{u'idBatiment': 2, u'etageSalle': u'6', u'numeroSalle': u'214', u'capaciteSalle': u'12'}]
-    return jsonify({'salle': result})
-
-@app.route('/SCSproject/api/setsalle', methods=['POST'])
-def create_salle():
-    as1 = ""
-    content = request.get_json(force = True)
-
-    print content
-    sallei = content.get("salle")
-    salle= sallei[0]
-    numeroSalle =  salle.get ("numeroSalle","")
-    etageSalle =  salle.get ("etageSalle","")
-    capaciteSalle =  salle.get ("capaciteSalle","")
-    equipementSalle =  salle.get ("equipementSalle","")
-    idBatiment =  salle.get ("idBatiment","")
-    InsertSalle(numeroSalle ,etageSalle, capaciteSalle, equipementSalle ,idBatiment )
-
-    return jsonify(result={"status": 200})
+    def ReturnSalle():
+        with app.app_context():
+            result = []
+            for data in collection.find():
+                result.append(data)
+            return result
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
 
-#result = []
-#result = [{'1': 1}, {'2': 2}]
-#jsonify({'result': result})
-#Insert Faculte
-#InsertNomFaculte ("UdeS" , "Sciences")
-#InsertNomFaculte ("UdeS" , "Math")
+    def InsertNomFaculte2 (nomUniversite , nomFaculte):
+        collection.update({"nomUniversite" : nomUniversite}, {"$addToSet":{"Faculte" : {"nomFaculte" : nomFaculte} } })
 
-#insert Batiment
-#InsertBatiment("UdeS", "Sciences", "Schulich", "425 Sherbrooke")
+    def InsertNomFaculte (nomUniversite , nomFaculte):
+    #    collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte.nomFaculte" : nomFaculte } })
+        collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte." + nomFaculte : nomFaculte } })
 
-#print collection
-#test = collection.find_one()
-#print test
+    def InsertBatiment2 (nomUniversite , nomFaculte, nomBatiment, adresseBatiment):
+    # add check nom existe pas deja
+    #    collection.update({"nomUniversite" : nomUniversite, "Faculte.nomFaculte": nomFaculte }, {"$push":{"(Faculte.1.nomFaculte)" : {"nomBatiment" : nomBatiment} } })
+    #    collection.update({"Faculte.nomFaculte": nomFaculte }, {"$push":{"(Faculte.1.nomFaculte)" : {"nomBatiment" : nomBatiment} } })
+    #    collection.update({"Faculte.nomFaculte": nomFaculte }, {"$push":{"Faculte" : { "Sciences" : { "nomBatiment" : nomBatiment} } } })
+        collection.update({"Faculte.nomFaculte": nomFaculte }, {"$push":{"Faculte" : { "elemMatch" : { "$in":  [1] } }} })
+
+    def InsertBatiment (nomUniversite , nomFaculte, nomBatiment, adresseBatiment):
+    #    collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte.nomFaculte" : nomFaculte } })
+        collection.update({"nomUniversite" : nomUniversite}, {"$set":{"Faculte." + nomFaculte + "." + nomBatiment : nomBatiment } })
+
+    ####################DRIVER###############################################
+
+
+
+
+    #result = []
+    #result = [{'1': 1}, {'2': 2}]
+    #jsonify({'result': result})
+    #Insert Faculte
+    #InsertNomFaculte ("UdeS" , "Sciences")
+    #InsertNomFaculte ("UdeS" , "Math")
+
+    #insert Batiment
+    #InsertBatiment("UdeS", "Sciences", "Schulich", "425 Sherbrooke")
+
+    #print collection
+    #test = collection.find_one()
+    #print test
+'''
