f13ef7061d7580c74da0576f8b6b584022beabf9
zakkar <fourat.djellouli@hotmail.com>
2015-10-19 04:31:57 +0200

    Login check

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,8 @@ class ContactForm(Form):
   subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
   message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
   submit = SubmitField("Send")
+
+class Authentification(Form):
+	login = TextField("Login",[validators.Required("Please enter your login")])
+	password = TextField("Password", [validators.Required("Please enter your password")])
+	submit = SubmitField("Send")
