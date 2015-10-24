98c7022b920eae94bdd6d199811542bad8ec176e
ppopops <ppopops@gmail.com>
2015-10-24 01:57:47 -0400

    added presentation and horaire

--- SCSproject.py
+++ SCSproject.py
@@ -3,6 +3,8 @@ from flask import Flask
 from MongoDB import Connect
 from Salle import *
 from Batiment import *
+from Presentation import *
+from Horaire import *
 app = Flask(__name__)
 
 db = Connect()
@@ -148,6 +150,168 @@ def delete_batiment():
 
     return jsonify(result={"status": 200})
 
+#######################################################################################################
+#Presentation
+#######################################################################################################
+@app.route('/SCSproject/api/setpresentation', methods=['POST'])
+def create_presentation():
+    content = request.get_json(force = True)
+    print content
+
+    presentationTemp = content.get("presentation")
+    presentation= presentationTemp[0]
+    titre =  presentation.get ("titre","")
+    nombreParticipants =  presentation.get ("nombreParticipants","")
+    equipement =  presentation.get ("equipement","")
+    duree =  presentation.get ("duree","")
+    presentateur =  presentation.get ("presentateur","")
+    id = "0"   #contructor will set id
+    presentation = Presentation( db, titre ,nombreParticipants, equipement, duree ,presentateur, id )
+    itemId = presentation.InsertPresentation()
+    #print itemId
+
+    return jsonify(presentation={"_id": itemId})
+    #return jsonify(result={"status": 200})
+
+@app.route('/SCSproject/api/getpresentation', methods=['GET'])
+def get_presentation():
+    presentation = Presentation(db)
+    return jsonify({'presentation': presentation.ReturnPresentation()})
+
+@app.route('/SCSproject/api/getpresentationid/<string:presentation_id>', methods=['GET'])
+def get_presentation_id(presentation_id):
+    presentation = Presentation(db)
+    return jsonify({'salle': presentation.ReturnPresentationid(presentation_id)})
+
+@app.route('/SCSproject/api/updatepresentation', methods=['POST'])
+def update_presentation():
+    content = request.get_json(force = True)
+    print content
+
+    presentationTemp = content.get("presentation")
+    presentation= presentationTemp[0]
+    titre =  presentation.get ("titre","")
+    nombreParticipants =  presentation.get ("nombreParticipants","")
+    equipement =  presentation.get ("equipement","")
+    duree =  presentation.get ("duree","")
+    presentateur =  presentation.get ("presentateur","")
+    id = presentation.get ("_id")
+    presentation = Presentation( db, titre ,nombreParticipants, equipement, duree , presentateur, id )
+    presentation.UpdatePresentation()
+
+    return jsonify(result={"status": 200})
+
+@app.route('/SCSproject/api/deletepresentation', methods=['POST'])
+def delete_presentation():
+    content = request.get_json(force = True)
+    print content
+
+    presentationTemp = content.get("presentation")
+    presentation= presentationTemp[0]
+    titre =  presentation.get ("titre","")
+    nombreParticipants =  presentation.get ("nombreParticipants","")
+    equipement =  presentation.get ("equipement","")
+    duree =  presentation.get ("duree","")
+    presentateur =  presentation.get ("presentateur","")
+    id = presentation.get ("_id")
+    presentation = Presentation( db, titre ,nombreParticipants, equipement, duree , presentateur, id )
+    presentation.DeletePresentation()
+
+    return jsonify(result={"status": 200})
+
+#######################################################################################################
+#Horaire
+#######################################################################################################
+@app.route('/SCSproject/api/sethoraire', methods=['POST'])
+def create_horaire():
+    content = request.get_json(force = True)
+    print content
+
+    horaireTemp = content.get("horaire")
+    horaire= horaireTemp[0]
+    date =  horaire.get ("date","")
+    idSalle =  horaire.get ("idSalle","")
+    idPresentation =  horaire.get ("idPresentation","")
+    heureDebut =  horaire.get ("heureDebut","")
+    id = "0"   #contructor will set id
+    horaire = Horaire( db, date ,idSalle, idPresentation, heureDebut, id )
+    itemId = horaire.InsertHoraire()
+    #print itemId
+
+    return jsonify(horaire={"_id": itemId})
+    #return jsonify(result={"status": 200})
+
+#@app.route('/SCSproject/api/getsalle', methods=['GET'])
+#def get_salle():
+#    salle = Salle(db)
+#    result = salle.ReturnSalle()
+
+#    i = 0
+#    for s in result :
+#        batiment = Batiment(db)
+#        batimentDocument = batiment.ReturnBatimentid(s["idBatiment"])
+#        result[i]["idBatiment"] = batimentDocument
+#        i = i+ 1
+
+    return jsonify({'salle': result})
+
+@app.route('/SCSproject/api/gethoraire', methods=['GET'])
+def get_horaire():
+    horaire = Horaire(db)
+    result = horaire.ReturnHoraire()
+
+    i=0
+    for s in result:
+        salle = Salle(db)
+        idSalle = salle.ReturnSalleid(s["idSalle"])
+        result[i]["idSalle"] = idSalle
+
+        presentation = Presentation(db)
+        idPresentation = presentation.ReturnPresentationid(s["idPresentation"])
+        result[i]["idPresentation"] = idPresentation
+
+        i = i +1
+    return jsonify ({'horaire' : result})
+    #return jsonify({'horaire': horaire.ReturnHoraire()})
+
+@app.route('/SCSproject/api/gethoraireid/<string:horaire_id>', methods=['GET'])
+def get_horaire_id(horaire_id):
+    horaire = Horaire(db)
+    return jsonify({'horaire': horaire.ReturnHoraireid(horaire_id)})
+
+@app.route('/SCSproject/api/updatehoraire', methods=['POST'])
+def update_horaire():
+    content = request.get_json(force = True)
+    print content
+
+    horaireTemp = content.get("horaire")
+    horaire= horaireTemp[0]
+    date =  horaire.get ("date","")
+    idSalle =  horaire.get ("idSalle","")
+    idPresentation =  horaire.get ("idPresentation","")
+    heureDebut =  horaire.get ("heureDebut","")
+    id = horaire.get ("_id")
+    horaire = Horaire( db, date ,idSalle, idPresentation, heureDebut, id )
+    horaire.UpdateHoraire()
+
+    return jsonify(result={"status": 200})
+
+@app.route('/SCSproject/api/deletehoraire', methods=['POST'])
+def delete_horaire():
+    content = request.get_json(force = True)
+    print content
+
+    horaireTemp = content.get("horaire")
+    horaire= horaireTemp[0]
+    date =  horaire.get ("date","")
+    idSalle =  horaire.get ("idSalle","")
+    idPresentation =  horaire.get ("idPresentation","")
+    heureDebut =  horaire.get ("heureDebut","")
+    id = horaire.get ("_id")
+    horaire = Horaire( db, date ,idSalle, idPresentation, heureDebut, id )
+    horaire.DeleteHoraire()
+
+    return jsonify(result={"status": 200})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
