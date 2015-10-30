3d14442227b979b192b3480e2843d809ff6f127b
ppopops <ppopops@gmail.com>
2015-10-29 23:25:15 -0400

    added user view added app.run (host='0.0.0.0')

--- Livrable01/application.py
+++ Livrable01/application.py
@@ -11,11 +11,13 @@ def create_app():
     from Database.Salle.views import salle_app
     from Database.Presentation.views import presentation_app
     from Database.Horaire.views import horaire_app
+    from Database.User.views import user_app
 
     # register blueprints
     app.register_blueprint(batiments_app)
     app.register_blueprint(salle_app)
     app.register_blueprint(presentation_app)
     app.register_blueprint(horaire_app)
+    app.register_blueprint(user_app)
 
     return app
