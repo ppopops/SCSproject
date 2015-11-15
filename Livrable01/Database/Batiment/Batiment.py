cd64671b68d101d1d30751c4336710a8de59c3da
ppopops <ppopops@gmail.com>
2015-11-14 19:14:18 -0500

    add return horaire disponible / non disponible

--- Livrable01/Database/Batiment/Batiment.py
+++ Livrable01/Database/Batiment/Batiment.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 
 class Batiment:
 
-    def __init__(self, db, nomBatiment = "0", adresseBatiment = "0", faculteBatiment="0", CampusBatiment = "0", id = "0"):
+    def __init__(self, db, nomBatiment = "0", adresseBatiment = "0", faculteBatiment="0", campusBatiment = "0", universiteBatiment = "0", id = "0"):
 
         self.collection = db.Batiment    # collection Batiment dans MongoDB
         if  id == "0":
@@ -17,10 +17,11 @@ class Batiment:
         self.nomBatiment = nomBatiment
         self.adresseBatiment = adresseBatiment
         self.faculteBatiment = faculteBatiment
-        self.CampusBatiment = CampusBatiment
+        self.campusBatiment = campusBatiment
+        self.universiteBatiment = universiteBatiment
 
     def InsertBatiment(self):
-        item = {"_id" : self.id, "nomBatiment" : self.nomBatiment ,"adresseBatiment" : self.adresseBatiment, "faculteBatiment" : self.faculteBatiment, "CampusBatiment" : self.CampusBatiment }
+        item = {"_id" : self.id, "nom" : self.nomBatiment ,"adresse" : self.adresseBatiment, "faculte" : self.faculteBatiment, "campus" : self.campusBatiment, "universite" : self.universiteBatiment }
         print item
         item_id = self.collection.insert_one(item).inserted_id
         print item_id
@@ -41,7 +42,7 @@ class Batiment:
 
     def UpdateBatiment(self):
         #print {"_id" : self.id, "numeroSalle" : self.numeroSalle ,"etageSalle" : self.etageSalle, "capaciteSalle" : self.capaciteSalle, "equipementSalle" : self.equipementSalle, "idBatiment" : self.idBatiment }
-        self.collection.update({"_id" : self.id}, {"nomBatiment" : self.nomBatiment ,"adresseBatiment" : self.adresseBatiment, "faculteBatiment" : self.faculteBatiment, "CampusBatiment" : self.CampusBatiment })
+        self.collection.update({"_id" : self.id}, {"nom" : self.nomBatiment ,"adresse" : self.adresseBatiment, "faculte" : self.faculteBatiment, "campus" : self.campusBatiment , "universite" : self.universiteBatiment })
         return
 
     def DeleteBatiment(self):
